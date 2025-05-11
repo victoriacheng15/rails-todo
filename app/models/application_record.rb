c4f7415bd6904b6ea0260c3bb95dd7305362ce2b
Victoria Cheng <victoria.cheng15@gmail.com>
2025-05-11 14:53:45 -0400

    chore: use ActiveModel instead of ActiveRecord... and updated Gemlock file

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
-class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+# class ApplicationRecord < ActiveRecord::Base
+#   primary_abstract_class
+# end
+
+class ApplicationRecord
+  include ActiveModel::Model  # Use ActiveModel for non-database models
 end
