63e15e1f7246c5f7998da940b6c1523e217e9c64
Victoria Cheng <victoria.cheng15@gmail.com>
2025-05-20 06:52:35 -0400

    chore: add posts controller, view and test

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+require "net/http"
+require "uri"
+require "json"
+
+class PostsController < ApplicationController
+  def index
+    @message = "Welcome to the Posts post!"
+    url = URI.parse("https://jsonplaceholder.typicode.com/posts")
+    response = Net::HTTP.get_response(url)
+
+    if response.is_a?(Net::HTTPSuccess)
+      @posts = JSON.parse(response.body)
+      puts @posts.inspect
+    else
+      @posts = []
+      flash[:error] = "Failed to fetch posts"
+    end
+  end
+
+  def posts_json
+    url = URI.parse("https://jsonplaceholder.typicode.com/posts")
+    response = Net::HTTP.get_response(url)
+
+    if response.is_a?(Net::HTTPSuccess)
+      posts = JSON.parse(response.body)
+      render json: posts
+    else
+      render json: { error: "Failed to fetch posts" }, status: :bad_gateway
+    end
+  end
+end
