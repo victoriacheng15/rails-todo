3850bf97e977980cf9113fb4b57e271ee6ed14de
Victoria Cheng <victoria.cheng15@gmail.com>
2025-05-20 06:58:50 -0400

    add posts (#7)
    * chore: uncomment apo only
    
    * chore: add routes for posts and posts_json
    
    * chore: revise css
    
    * chore: cleanup contents on home page
    
    * chore: add posts controller, view and test
    
    * fix: correct posts url
    
    * chore: rename posts_json to json to make it standard for home page

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module TodoApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.api_only = true
+    # config.api_only = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
   get "home_json", to: "home#json"
+  get "posts", to: "posts#index"
+  get "posts_json", to: "posts#json"
 end
